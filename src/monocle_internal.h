c1712737b62fdf227bdab2ee5ffe631215867900
Michael Martin <mcmartin@gmail.com>
2013-07-28 18:23:34 -0700

    Decoration for C-style headers, just in case

--- src/monocle_internal.h
+++ src/monocle_internal.h
@@ -1,8 +1,16 @@
 #ifndef MONOCLE_INTERNAL_H_
 #define MONOCLE_INTERNAL_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* This isn't really exciting, it just defines some symbols used cross-subsystem but not exported. */
 
 void mncl_uninit_raw_system(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
